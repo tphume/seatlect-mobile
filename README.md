5fb4ad3fc4c4ed1a72a73c4fa400fb9c023cd258
Traphume <traphumedev@gmail.com>
2020-11-15 15:04:14 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# seatlect_mobile
+
+User mobile client for Seatlect system.
+
---
+++ android/app/src/main/kotlin/com/tphume/seatlect_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tphume.seatlect_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
