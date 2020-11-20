556ec07cd81984f563937e23ef04f885ec7525a9
Traphume <traphumedev@gmail.com>
2020-11-20 22:54:26 +0700

    Added empty user and business entity to entity pkg

---
+++ packages/entity/lib/src/business.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+class Business extends Equatable {
+  Business();
+
+  @override
+  List<Object> get props {
+    return [];
+  }
+}
---
+++ packages/entity/lib/src/user.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  User();
+
+  @override
+  List<Object> get props {
+    return [];
+  }
+}
