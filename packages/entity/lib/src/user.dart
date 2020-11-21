fc919f228d0fd1725fe2e1db704f8ffd53fec207
Traphume <traphumedev@gmail.com>
2020-11-21 17:19:53 +0700

    Added fields to user entity

--- packages/entity/lib/src/user.dart
+++ packages/entity/lib/src/user.dart
@@ -1,11 +1,29 @@
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
 
+import 'entity.dart';
+
 class User extends Equatable {
-  User();
+  final String username;
+  final DateTime dob;
+  final String avatar;
+  final List<String> preference;
+  final List<Business> favorite;
+
+  User(
+      {@required this.username,
+      @required this.dob,
+      @required this.avatar,
+      @required this.preference,
+      @required this.favorite})
+      : assert(username != null),
+        assert(dob != null),
+        assert(avatar != null),
+        assert(preference != null),
+        assert(favorite != null);
 
   @override
   List<Object> get props {
-    return [];
+    return [username, dob, avatar, preference, favorite];
   }
 }
