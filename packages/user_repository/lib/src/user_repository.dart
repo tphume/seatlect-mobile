19659bd03184267ceb7b7145d95bcb8db11ea2a7
Traphume <traphumedev@gmail.com>
2020-11-22 10:48:59 +0700

    Added stream to user repo class

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -2,6 +2,26 @@ import 'dart:async';
 
 import 'package:entity/entity.dart';
 
-class UserRepo {}
+class UserRepo {
+  User _user;
+  final _controller = StreamController<User>();
 
-class MockUserRepo implements UserRepo {}
+  Stream<User> get user async* {
+    yield _user;
+    yield* _controller.stream;
+  }
+
+  void dispose() => _controller.close();
+}
+
+class MockUserRepo implements UserRepo {
+  User _user;
+  final _controller = StreamController<User>();
+
+  Stream<User> get user async* {
+    yield _user;
+    yield* _controller.stream;
+  }
+
+  void dispose() => _controller.close();
+}
