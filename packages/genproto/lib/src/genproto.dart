459cd31d6350eee19b40cacb86edb5fb49e6df41
Traphume <traphumedev@gmail.com>
2020-11-22 18:25:28 +0700

    Added export

--- packages/genproto/lib/src/genproto.dart
+++ packages/genproto/lib/src/genproto.dart
@@ -1,1 +1,10 @@
-// Export only client stubs
+// Common types
+export 'common.pb.dart';
+
+// User service
+export 'user.pb.dart';
+export 'user.pbgrpc.dart';
+
+// Token service
+export 'token.pb.dart';
+export 'token.pbgrpc.dart';
