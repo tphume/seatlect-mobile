5afe97b99d9ac251fafa0868047cb7d83fbf452c
Traphume <traphumedev@gmail.com>
2020-11-22 19:12:39 +0700

    Added user_repo client

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -1,10 +1,20 @@
 import 'dart:async';
+import 'package:meta/meta.dart';
 
 import 'package:entity/entity.dart';
+import 'package:genproto/genproto.dart' as api;
 
 class UserRepo {
+  // This is used to communicate with the presentation layer of the application
   User _user;
+
+  // For pushing updates to the presentation layer
   final _controller = StreamController<User>();
+
+  // Client for calling gRPC endpoint
+  api.UserServiceClient client;
+
+  UserRepo({@required this.client}) : assert(client != null);
 
   Stream<User> get user async* {
     yield _user;
@@ -21,8 +31,16 @@ class UserRepo {
 }
 
 class MockUserRepo implements UserRepo {
+  // This is used to communicate with the presentation layer of the application
   User _user;
+
+  // For pushing updates to the presentation layer
   final _controller = StreamController<User>();
+
+  // Client for calling gRPC endpoint - mock doesn't actually use this
+  api.UserServiceClient client;
+
+  MockUserRepo({@required this.client});
 
   Stream<User> get user async* {
     yield _user;
