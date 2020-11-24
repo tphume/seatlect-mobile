be629f3407129b100dd511686f43818debdfd118
Traphume <traphumedev@gmail.com>
2020-11-24 21:15:51 +0700

    Added empty and populated state to user bloc

--- lib/user/bloc/user_bloc.dart
+++ lib/user/bloc/user_bloc.dart
@@ -3,12 +3,14 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:entity/entity.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
 part 'user_event.dart';
+
 part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserInitial());
+  UserBloc() : super(UserEmpty());
 
   @override
   Stream<UserState> mapEventToState(
--- lib/user/bloc/user_state.dart
+++ lib/user/bloc/user_state.dart
@@ -1,10 +1,25 @@
 part of 'user_bloc.dart';
 
 abstract class UserState extends Equatable {
-  const UserState();
+  final User user;
+
+  const UserState({this.user});
 }
 
-class UserInitial extends UserState {
+class UserEmpty extends UserState {
+  final User user = User.empty;
+
+  const UserEmpty();
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [user];
+}
+
+class UserPopulated extends UserState {
+  final User user;
+
+  const UserPopulated(this.user);
+
+  @override
+  List<Object> get props => [user];
 }
