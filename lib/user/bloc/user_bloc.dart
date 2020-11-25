79faccf7447c61184dbcd7af62d698dd2923237a
Traphume <traphumedev@gmail.com>
2020-11-25 12:48:20 +0700

    Added listening to user repo stream

--- lib/user/bloc/user_bloc.dart
+++ lib/user/bloc/user_bloc.dart
@@ -3,14 +3,24 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:entity/entity.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+import 'package:user_repository/user_repository.dart';
 
 part 'user_event.dart';
 
 part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserEmpty());
+  final UserRepo userRepo;
+  StreamSubscription<User> userSubscription;
+
+  UserBloc({@required this.userRepo})
+      : assert(userRepo != null),
+        super(UserEmpty()) {
+    this.userSubscription = userRepo.user.listen((user) {
+      add(UserStateChange(user: user));
+    });
+  }
 
   @override
   Stream<UserState> mapEventToState(
@@ -23,5 +33,12 @@ class UserBloc extends Bloc<UserEvent, UserState> {
         yield UserPopulated(user: event.user);
       }
     }
+  }
+
+  @override
+  Future<Function> close() {
+    userSubscription?.cancel();
+    this.userRepo.dispose();
+    return super.close();
   }
 }
