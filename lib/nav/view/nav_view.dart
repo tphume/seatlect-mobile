b646354d1d0784bfc0c4ad0c065c7da05b21f9ec
Traphume <traphumedev@gmail.com>
2020-11-29 11:23:14 +0700

    Added placeholder nav appbar and drawer

--- lib/nav/view/nav_view.dart
+++ lib/nav/view/nav_view.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class NavAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('Placeholder for now'));
+  }
+}
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
