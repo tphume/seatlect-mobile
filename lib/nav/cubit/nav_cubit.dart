69b2e6a9fd9db739eee5f2158a7ea57089e53be2
Traphume <traphumedev@gmail.com>
2020-11-29 10:57:42 +0700

    Added state and method to nav cubit

--- lib/nav/cubit/nav_cubit.dart
+++ lib/nav/cubit/nav_cubit.dart
@@ -4,5 +4,7 @@ import 'package:meta/meta.dart';
 part 'nav_state.dart';
 
 class NavCubit extends Cubit<NavState> {
-  NavCubit() : super(NavInitial());
+  NavCubit() : super(NavHome());
+
+  void switchPage(NavState page) => emit(page);
 }
--- lib/nav/cubit/nav_state.dart
+++ lib/nav/cubit/nav_state.dart
@@ -3,4 +3,10 @@ part of 'nav_cubit.dart';
 @immutable
 abstract class NavState {}
 
-class NavInitial extends NavState {}
+class NavHome extends NavState {}
+
+class NavSearch extends NavState {}
+
+class NavOrder extends NavState {}
+
+class NavSettings extends NavState {}
