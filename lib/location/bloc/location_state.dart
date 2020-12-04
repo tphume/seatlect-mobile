7a6814efc442427413c57759bb700261ce5227b6
Traphume <traphumedev@gmail.com>
2020-12-04 13:08:31 +0700

    Added Selected Location state to bloc

--- lib/location/bloc/location_state.dart
+++ lib/location/bloc/location_state.dart
@@ -2,9 +2,22 @@ part of 'location_cubit.dart';
 
 abstract class LocationState extends Equatable {
   const LocationState();
-}
 
-class LocationInitial extends LocationState {
   @override
   List<Object> get props => [];
+}
+
+// Only use on init
+class LocationInitial extends LocationState {}
+
+// Contains user selected location - or current location
+class LocationSelected extends LocationState {
+  final String location;
+
+  LocationSelected({@required this.location}) : assert(location != null);
+
+  @override
+  List<Object> get props {
+    return [location];
+  }
 }
