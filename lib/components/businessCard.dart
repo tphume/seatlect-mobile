0a2a10b8888790e4f51abde36e73cab77af88469
Traphume <traphumedev@gmail.com>
2020-12-08 21:31:32 +0700

    Added text to business card

--- lib/components/businessCard.dart
+++ lib/components/businessCard.dart
@@ -8,8 +8,10 @@ class BusinessCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Container(
-      height: 100,
+      padding: EdgeInsets.only(left: 10, top: 12, right: 10, bottom: 10),
       margin: EdgeInsets.only(bottom: 15),
       decoration: BoxDecoration(
           color: Color(0xffE4E0EF),
@@ -17,7 +19,34 @@ class BusinessCard extends StatelessWidget {
           boxShadow: [
             BoxShadow(color: Color(0xFFEAEAEA), blurRadius: 4, spreadRadius: 5)
           ]),
-      child: Row(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 90,
+            width: 90,
+            decoration: BoxDecoration(
+              color: theme.primaryColorLight,
+              borderRadius: BorderRadius.circular(13),
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                business.name,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Text(business.address),
+              Text(business.tags
+                  .map<String>((e) => '${e[0]}${e.substring(1).toLowerCase()}')
+                  .toString()
+                  .replaceAll('(', '')
+                  .replaceAll(')', ''))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
