fe5af3e05a718132b0b8e069bdfe3a3680b62c94
Traphume <traphumedev@gmail.com>
2020-12-08 21:48:04 +0700

    Styled business card text

--- lib/components/businessCard.dart
+++ lib/components/businessCard.dart
@@ -1,5 +1,6 @@
 import 'package:entity/entity.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BusinessCard extends StatelessWidget {
   final Business business;
@@ -30,20 +31,43 @@ class BusinessCard extends StatelessWidget {
               borderRadius: BorderRadius.circular(13),
             ),
           ),
-          Column(
-            children: [
-              Text(
-                business.name,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              ),
-              Text(business.address),
-              Text(business.tags
-                  .map<String>((e) => '${e[0]}${e.substring(1).toLowerCase()}')
-                  .toString()
-                  .replaceAll('(', '')
-                  .replaceAll(')', ''))
-            ],
+          SizedBox(
+            height: 90,
+            width: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  business.name,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: GoogleFonts.yantramanav(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 21,
+                      color: Color(0xFF1F1F24)),
+                ),
+                Text(
+                  business.address,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: GoogleFonts.yantramanav(
+                      fontWeight: FontWeight.w400, color: Color(0xFF4F4F4F)),
+                ),
+                Text(
+                  business.tags
+                      .map<String>(
+                          (e) => '${e[0]}${e.substring(1).toLowerCase()}')
+                      .toString()
+                      .replaceAll('(', '')
+                      .replaceAll(')', ''),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: GoogleFonts.yantramanav(
+                      fontWeight: FontWeight.w400, color: Color(0xFF4F4F4F)),
+                )
+              ],
+            ),
           )
         ],
       ),
