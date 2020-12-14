6af169101cf52b0d84c1f5f37630f0460a4a2f14
Traphume <traphumedev@gmail.com>
2020-12-14 13:40:01 +0700

    Added favorite event handlers

--- lib/favorites/bloc/favorite_bloc.dart
+++ lib/favorites/bloc/favorite_bloc.dart
@@ -3,20 +3,48 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
-
 import 'package:entity/entity.dart';
+
+import 'package:user_repository/user_repository.dart';
+import 'package:business_repository/business_repository.dart';
 
 part 'favorite_event.dart';
 
 part 'favorite_state.dart';
 
 class FavoriteBloc extends Bloc<FavoriteEvent, FavoriteState> {
-  FavoriteBloc() : super(FavoriteInitial());
+  final UserRepo userRepo;
+  final BusinessRepo businessRepo;
+
+  FavoriteBloc({@required this.userRepo, @required this.businessRepo})
+      : assert(userRepo != null),
+        assert(businessRepo != null),
+        super(FavoriteInitial());
 
   @override
   Stream<FavoriteState> mapEventToState(
     FavoriteEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is FetchFavorite) {
+      try {
+        final businesses = await businessRepo.ListBusinessById(event.ids);
+        yield FavoriteCurrent(businesses: businesses);
+      } catch (e) {}
+    } else if (event is AddFavorite) {
+      try {
+        await userRepo.AddFavorite(event.business.id);
+
+        final businesses = [...state.businesses, event.business];
+        yield FavoriteCurrent(businesses: businesses);
+      } catch (e) {}
+    } else if (event is RemoveFavorite) {
+      try {
+        await userRepo.RemoveFavorite(event.business.id);
+
+        final businesses = state.businesses;
+        businesses.removeWhere((b) => b.id == event.business.id);
+        yield FavoriteCurrent(businesses: businesses);
+      } catch (e) {}
+    }
   }
 }
--- lib/favorites/bloc/favorite_state.dart
+++ lib/favorites/bloc/favorite_state.dart
@@ -1,27 +1,22 @@
 part of 'favorite_bloc.dart';
 
 abstract class FavoriteState extends Equatable {
-  final List<String> businessIds = [];
   final List<Business> businesses = [];
 
   FavoriteState();
 
   @override
   List<Object> get props {
-    return [this.businessIds, this.businesses];
+    return [this.businesses];
   }
 }
 
 class FavoriteInitial extends FavoriteState {
-  final List<String> businessIds = [];
   final List<Business> businesses = [];
 }
 
 class FavoriteCurrent extends FavoriteState {
-  final List<String> businessIds;
   final List<Business> businesses;
 
-  FavoriteCurrent({@required this.businessIds, @required this.businesses})
-      : assert(businessIds != null),
-        assert(businesses != null);
+  FavoriteCurrent({@required this.businesses}) : assert(businesses != null);
 }
