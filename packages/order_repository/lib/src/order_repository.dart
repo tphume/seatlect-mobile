65979c1ecd1e8ec9d7ea1a4153493954d9bd072c
Traphume <traphumedev@gmail.com>
2020-12-18 21:58:56 +0700

    Implemented Order repository

---
+++ packages/order_repository/lib/src/order_repository.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+
+import 'package:meta/meta.dart';
+import 'package:entity/entity.dart';
+import 'package:genproto/genproto.dart' as api;
+
+class OrderRepo {
+  // Client for calling gRPC endpoint
+  api.OrderServiceClient client;
+
+  OrderRepo({@required this.client}) : assert(client != null);
+
+  Future<List<Order>> ListOrder(int limit, int page) async {
+    // Construct request object
+    final request = api.ListOrderRequest()
+      ..limit = limit
+      ..page = page;
+
+    // Call endpoint
+    try {
+      final response = await client.listOrder(request);
+      final orders =
+          response.orders.map<Order>((o) => OrderProtoToEntity(o)).toList();
+
+      return orders;
+    } catch (e) {
+      developer.log('non-gRPC error at login',
+          time: DateTime.now(), name: 'UserRepo', error: e);
+
+      rethrow;
+    }
+  }
+}
+
+Order OrderProtoToEntity(api.Order o) {
+  return Order(
+      id: o.id,
+      reservationId: o.reservationId,
+      businessId: o.businessId,
+      start: DateTime.parse(o.start),
+      end: DateTime.parse(o.end),
+      seats: o.seats
+          .map<Seat>((s) => Seat(
+              name: s.name,
+              floor: 0,
+              space: s.space,
+              price: s.price,
+              x: 0,
+              y: 0))
+          .toList(),
+      preorder: o.preorder.map<Preorder>((p) => Preorder(
+          name: p.name,
+          description: p.description,
+          image: p.image,
+          price: p.price,
+          quantity: p.quantity)),
+      totalPrice: o.totalPrice,
+      status: o.status,
+      image: o.image,
+      extraSpace: o.extraSpace);
+}
