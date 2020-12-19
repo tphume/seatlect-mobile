6bba448941f032e9d40513f6dc03387256f197b4
Traphume <traphumedev@gmail.com>
2020-12-19 15:46:58 +0700

    Added order bloc to app

--- lib/order/view/order_page.dart
+++ lib/order/view/order_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import 'package:seatlect_mobile/order/bloc/order_bloc.dart';
 import 'package:seatlect_mobile/components/drawer_content.dart' as comp;
 
 class OrderPage extends StatelessWidget {
@@ -11,14 +14,29 @@ class OrderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Order',
+            style: GoogleFonts.dmSans(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w600)),
         iconTheme: IconThemeData(color: Colors.black),
         backgroundColor: Colors.transparent,
+        centerTitle: true,
         elevation: 0,
       ),
       drawer: Drawer(
         child: comp.DrawerContent(comp.Page.order),
       ),
-      body: Text('Order'),
+      body: _buildOrders(),
     );
+  }
+
+  Widget _buildOrders() {
+    return BlocBuilder<OrderBloc, OrderState>(
+        builder: (context, state) => ListView(
+              children: state.orders
+                  .map<Widget>((o) => Text(o.reservationId))
+                  .toList(),
+            ));
   }
 }
