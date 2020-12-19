cbae1982bd9c0911f33b0b49d1e7460b1329c410
Traphume <traphumedev@gmail.com>
2020-12-19 18:07:34 +0700

    Added text to order card

--- lib/order/view/order_card.dart
+++ lib/order/view/order_card.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:entity/entity.dart';
 import 'package:flutter/material.dart';
 
+final weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
 class OrderCard extends StatelessWidget {
   final Order order;
 
@@ -12,26 +14,64 @@ class OrderCard extends StatelessWidget {
     final theme = Theme.of(context);
 
     return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          gradient:
-              LinearGradient(colors: [theme.accentColor, Color(0xFFFED37F)])),
-      child: Row(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: CachedNetworkImage(
-                imageUrl: order.business.displayImage,
-                fit: BoxFit.cover,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            gradient:
+                LinearGradient(colors: [theme.accentColor, Color(0xFFFED37F)])),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: CachedNetworkImage(
+                  imageUrl: order.business.displayImage,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          )
-        ],
-      ),
-    );
+            Container(
+              padding: EdgeInsets.all(4),
+              height: 100,
+              width: 225,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          order.business.name,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18,
+                              color: Color(0xFF1F1F24)),
+                        ),
+                        Text(
+                          order.seats.map((s) => s.name).join(','),
+                          style: TextStyle(fontSize: 16),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      ]),
+                  Text(
+                    _buildDate(order.start),
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+
+  String _buildDate(DateTime d) {
+    return '${weekdays[d.weekday - 1]}. ${d.day}-${d.month}-${d.year}';
   }
 }
