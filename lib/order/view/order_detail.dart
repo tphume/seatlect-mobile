da38ac1c1b0ea8f410e734654effa2745360f589
Traphume <traphumedev@gmail.com>
2020-12-20 21:15:15 +0700

    Added row builder function for ticket

--- lib/order/view/order_detail.dart
+++ lib/order/view/order_detail.dart
@@ -43,11 +43,78 @@ class OrderDetail extends StatelessWidget {
                   padding: EdgeInsets.all(20),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text('hello')],
+                    children: [
+                      Container(
+                          child: Text(
+                        order.business.name,
+                        style: TextStyle(
+                            color: theme.primaryColor,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 19),
+                      )),
+                      _buildRow(context,
+                          firstTitle: 'Date',
+                          firstContent:
+                              '${order.start.day}-${order.start.month}-${order.start.year}',
+                          secondTitle: 'Seat',
+                          secondContent:
+                              order.seats.map((s) => s.name).join(',')),
+                      Row(),
+                      Row()
+                    ],
                   ),
                 ))
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildRow(context,
+      {@required String firstTitle,
+      @required String firstContent,
+      @required String secondTitle,
+      @required String secondContent}) {
+    final theme = Theme.of(context);
+
+    final contentStyle = TextStyle(
+        fontWeight: FontWeight.w700, fontSize: 19, color: theme.primaryColor);
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+              width: 150,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(firstTitle,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Color(0xFF828282))),
+                  Text(firstContent, style: contentStyle)
+                ],
+              )),
+          Container(
+              width: 150,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(secondTitle,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Color(0xFF828282))),
+                  Text(
+                    secondContent,
+                    style: contentStyle,
+                  )
+                ],
+              )),
+        ],
       ),
     );
   }
