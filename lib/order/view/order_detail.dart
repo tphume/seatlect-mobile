249e3df9f55527f1417ff574461533653e59cf16
Traphume <traphumedev@gmail.com>
2020-12-20 20:13:43 +0700

    Added layour container to order detail page

--- lib/order/view/order_detail.dart
+++ lib/order/view/order_detail.dart
@@ -14,12 +14,27 @@ class OrderDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final media = MediaQuery.of(context);
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
         iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Container(
+        height: media.size.height,
+        width: media.size.width,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFFFED37F), Color(0xFFCFBEED)])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Container()],
+        ),
       ),
     );
   }
