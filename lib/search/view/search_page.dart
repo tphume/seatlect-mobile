399ad15abd1a7bfa42650be523e81512b1715114
Traphume <traphumedev@gmail.com>
2021-01-05 14:19:25 +0700

    Added skeleton layout to search page

--- lib/search/view/search_page.dart
+++ lib/search/view/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:seatlect_mobile/components/drawer_content.dart' as comp;
 
@@ -10,15 +11,25 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      drawer: Drawer(
-        child: comp.DrawerContent(comp.Page.search),
-      ),
-      body: Text('Search'),
-    );
+        appBar: AppBar(
+          title: Text(
+            'Search',
+            style: GoogleFonts.dmSans(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+          iconTheme: IconThemeData(color: Colors.black),
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          elevation: 0,
+        ),
+        drawer: Drawer(
+          child: comp.DrawerContent(comp.Page.search),
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(left: 15, top: 10, right: 15, bottom: 10),
+          child: Column(
+            children: [],
+          ),
+        ));
   }
 }
