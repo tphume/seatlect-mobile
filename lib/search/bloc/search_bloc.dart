48cf0074dc6a5625def8b3f2e7169f5b9f389019
Traphume <traphumedev@gmail.com>
2021-01-05 15:12:58 +0700

    Added search bloc to app

--- lib/search/bloc/search_bloc.dart
+++ lib/search/bloc/search_bloc.dart
@@ -1,17 +1,22 @@
 import 'dart:async';
-import 'dart:collection';
 
 import 'package:bloc/bloc.dart';
+import 'package:business_repository/business_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 import 'package:entity/entity.dart';
 
 part 'search_event.dart';
+
 part 'search_state.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
-  SearchBloc() : super(SearchInitial());
+  final BusinessRepo businessRepo;
+
+  SearchBloc({@required this.businessRepo})
+      : assert(businessRepo != null),
+        super(SearchInitial());
 
   @override
   Stream<SearchState> mapEventToState(
