f1d80e3fa351352b37fe83d103f98d0892829a20
Traphume <traphumedev@gmail.com>
2021-01-06 20:10:12 +0700

    Added search by name to search page

--- lib/search/view/search_page.dart
+++ lib/search/view/search_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:seatlect_mobile/components/drawer_content.dart' as comp;
+import 'package:seatlect_mobile/search/bloc/search_bloc.dart';
 
 class SearchPage extends StatelessWidget {
   static Route route() {
@@ -28,8 +30,35 @@ class SearchPage extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.only(left: 15, top: 10, right: 15, bottom: 10),
           child: Column(
-            children: [],
+            children: [
+              Container(
+                child: _buildNameFilter(context),
+              )
+            ],
           ),
         ));
+  }
+
+  Widget _buildNameFilter(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return BlocBuilder<SearchBloc, SearchState>(builder: (context, state) {
+      return TextFormField(
+        style: TextStyle(fontSize: 16),
+        decoration: InputDecoration(
+            prefixIcon: Icon(
+              Icons.search,
+              color: theme.primaryIconTheme.color,
+            ),
+            hintText: 'Search by name',
+            hintStyle: TextStyle(fontSize: 16, color: Color(0xFFCFBEED)),
+            contentPadding: EdgeInsets.all(8),
+            fillColor: Color(0xFFE4E0EF),
+            filled: true,
+            border: OutlineInputBorder(
+                borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                borderRadius: BorderRadius.circular(20))),
+      );
+    });
   }
 }
